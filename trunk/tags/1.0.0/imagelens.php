97afac88762e3d7a8a9bc78fb09230af14097e57
webdevmattcrom <webdevmattcrom@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-31 09:17:54 +0000

    Fixing tags and merging.
    git-svn-id: https://plugins.svn.wordpress.org/imagelens@976040 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ trunk/tags/1.0.0/imagelens.php
@@ -0,0 +1,22 @@
+<?php
+/*
+Plugin Name: Imagelens
+Plugin URI: http://www.ramoonus.nl
+Description: A jQuery plug-in for Lens Effect Image Zooming
+Version: 1.0.0
+Author: Ramoonus
+Author URI: http://www.ramoonus.nl/wordpress/imagelens/
+*/
+
+// Install
+function rw_imagelens() {
+		// load jQuery
+		wp_enqueue_script('jquery'); // load
+		// Load Imagelens
+		wp_deregister_script('imagelens'); // deregister
+		wp_register_script('imagelens', plugins_url('/js/jquery.imageLens.js', __FILE__), false, '1.0.0', false); // re register // false for not in footer
+		wp_enqueue_script('imagelens'); // load
+}
+// init
+add_action('init', 'rw_imagelens'); 
+?>
---
+++ trunk/trunk/admin/settings/faqs.php
@@ -0,0 +1,31 @@
+<div data-collapse="accordion" id="faq">			
+			<h2><div class="dashicons dashicons-editor-help"></div><?php _e('How does this thing work?', 'mc-imagelens'); ?></h2>
+			<div class="content"><div class="dashicons dashicons-yes"></div>
+			<p><?php _e('Once ImageLens is enabled, you\'ll notice a box below your post and page editor that says: "Enable ImageLens for this Content Area?" That is where all the magic is. Once you check that checkbox, you\'ll be given choices to customize the look and functionality of ImageLens for that specific post/page. Here\'s a quick summary of your options:', 'mc-imagelens'); ?></p><ul><li><?php _e('<strong>ALL IMAGES</strong>: With this option you can enable all the images you place into this post or page to have the ImageLens effect, even galleries.', 'mc-imagelens'); ?></li><li><?php _e('<strong>PER IMAGE</strong>: If you prefer to have choose specific images in your post/page that have the ImageLens effect, choose this option. Then when you go to add your image, make sure to choose "Yes" in the "Enable ImageLens" selector.', 'mc-imagelens'); ?></li></ul>
+			</div>
+			
+			<h2><div class="dashicons dashicons-editor-help"></div><?php _e('Why isn\'t ImageLens working on my images after I activate the plugin?', 'mc-imagelens'); ?></h2>
+			<div class="content"><div class="dashicons dashicons-yes"></div>
+			<p><?php _e('ImageLens hooks to your images with a custom class. That means that only images you have inserted after you activated ImageLens will work with the selective method. You can choose "All Images" and that will work for that page no problem. But if you want individual images, you\'ll have to re-insert them into the page in order for the right class name to be added to that image.', 'mc-imagelens'); ?></p>
+			</div>
+			
+			<h2><div class="dashicons dashicons-editor-help"></div><?php _e('The lens appears, but my it\'s not really zoomed well. What\'s the deal!?', 'mc-imagelens'); ?></h2>
+			<div class="content"><div class="dashicons dashicons-yes"></div>
+			<p><?php _e('The way ImageLens works, you have to insert a large image and have is resized smaller on the page in order to get a good magnification effect. It\'s best to insert the full image size, then manually drag the image smaller in the editor after you insert it. The smaller the image is compared to the original, the higher the magnification.', 'mc-imagelens'); ?></p>
+			</div>
+			
+			<h2><div class="dashicons dashicons-editor-help"></div><?php _e('It doesn\'t seem to be working on my CPT\'s or on WooCommerce images.', 'mc-imagelens'); ?></h2>
+			<div class="content"><div class="dashicons dashicons-yes"></div>
+			<p><?php _e('That\'s right. Currently ImageLens only works on Posts and Pages. I plan to implement it for CPT\'s and e-commerce product pages in the future.', 'mc-imagelens'); ?></p>
+			</div>
+			
+			<h2><div class="dashicons dashicons-editor-help"></div><?php _e('I\'d like to use this in a sidebar widget. How do I do that?.', 'mc-imagelens'); ?></h2>
+			<div class="content"><div class="dashicons dashicons-yes"></div>
+			<p><?php _e('For now, you can enable the "Selective" option on the page where you\'re widget will appear, then on the image you want to use add the class "imagelens-single", and it\'ll work just dandy.', 'mc-imagelens'); ?></p>
+			</div>
+			
+			<h2><div class="dashicons dashicons-editor-help"></div><?php _e('This is JUST want I needed? How can I ever thank you!?' , 'mc-imagelens'); ?></h2>
+			<div class="content"><div class="dashicons dashicons-yes"></div>
+			<p><?php _e('Hey, that\'s what I\'m here for. But if you\'d really like to thank me, check out my <a href="http://mattcromwell.com" target="_blank">website</a>, subscribe to my newsletter, tweet about how much you like the plugin, or feel free to donate here.', 'mc-imagelens'); ?></p>
+			</div>
+</div>
