bf24aa76cb5be54aea9c5466d03fe99554c078fa
webdevmattcrom <webdevmattcrom@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-08-31 09:09:18 +0000

    Major Upgrade to version 2.0.0. Whoohoo!
    git-svn-id: https://plugins.svn.wordpress.org/imagelens@976039 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tags/1.0.0/trunk/js/imagelens.js
@@ -0,0 +1,81 @@
+/*  
+    http://www.dailycoding.com/ 
+*/
+(function ($) {
+    $.fn.imageLens = function (options) {
+
+        var defaults = {
+            lensSize: 100,
+            borderSize: 4,
+            borderColor: "#888",
+			lensCss: 'the-lens',
+			addInset: false,
+			addShadow: false,
+			Inset: 'null',
+			Shadow: 'null',
+        };
+		
+        var options = $.extend(defaults, options);
+        
+		if (options.addInset == true){
+			options.Inset = 'webkit-box-shadow: inset 1px 0px 31px 2px rgba(0,0,0,0.75); -moz-box-shadow: inset 1px 0px 31px 2px rgba(0,0,0,0.75); box-shadow: inset 1px 0px 31px 2px rgba(0,0,0,0.75);';
+		}
+		
+		if (options.addShadow == true){
+			options.Shadow = '-webkit-box-shadow: 1px 0px 31px 2px rgba(0,0,0,0.75);-moz-box-shadow: 1px 0px 31px 2px rgba(0,0,0,0.75);box-shadow: 1px 0px 31px 2px rgba(0,0,0,0.75);';
+		}
+		
+		var lensStyle = "background-position: 0px 0px;width: " + String(options.lensSize) + "px;height: " + String(options.lensSize)
+            + "px;float: left;display: none;border-radius: " + String(options.lensSize / 2 + options.borderSize)
+            + "px;border: " + String(options.borderSize) + "px solid " + options.borderColor 
+            + ";background-repeat: no-repeat;position: absolute; cursor: none;" + options.Inset + options.Shadow;
+		
+        return this.each(function () {
+            obj = $(this);
+
+            var offset = $(this).offset();
+
+            // Creating lens
+            var target = $("<div style='" + lensStyle + "' class='" + options.lensCss + "'>&nbsp;</div>").appendTo($("body"));
+            var targetSize = target.size();
+
+            // Calculating actual size of image
+            var imageSrc = options.imageSrc ? options.imageSrc : $(this).attr("src");
+            var imageTag = "<img style='display:none;' src='" + imageSrc + "' />";
+
+            var widthRatio = 0;
+            var heightRatio = 0;
+
+            $(imageTag).load(function () {
+                widthRatio = $(this).width() / obj.width();
+                heightRatio = $(this).height() / obj.height();
+            }).appendTo($(this).parent());
+
+            target.css({ backgroundImage: "url('" + imageSrc + "')" });
+
+            target.mousemove(setPosition);
+            $(this).mousemove(setPosition);
+
+            function setPosition(e) {
+
+                var leftPos = parseInt(e.pageX - offset.left);
+                var topPos = parseInt(e.pageY - offset.top);
+
+                if (leftPos < 0 || topPos < 0 || leftPos > obj.width() || topPos > obj.height()) {
+                    target.hide();
+                }
+                else {
+                    target.show();
+
+                    leftPos = String(((e.pageX - offset.left) * widthRatio - target.width() / 2) * (-1));
+                    topPos = String(((e.pageY - offset.top) * heightRatio - target.height() / 2) * (-1));
+                    target.css({ backgroundPosition: leftPos + 'px ' + topPos + 'px' });
+
+                    leftPos = String(e.pageX - target.width() / 2);
+                    topPos = String(e.pageY - target.height() / 2);
+                    target.css({ left: leftPos + 'px', top: topPos + 'px' });
+                }
+            }
+        });
+    };
+})(jQuery);
